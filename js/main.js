5016945f5e9663b4a337661e9d135aca45cd9335
MuhummadShohag <muhammadshohagislam.software.e@gmail.com>
2022-08-21 09:57:23 +0300

    Done Money Master Project

--- js/main.js
+++ js/main.js
@@ -1,22 +1,55 @@
+// calculating expenses and balance
 document.getElementById("calculate-btn").addEventListener("click", function () {
     /* Total Income */
-    // get income input field
-    const incomeInputField = document.getElementById("incomeInput");
-    const incomeInputFieldString = incomeInputField.value;
-    // string to number
-    const incomeInputFieldValue = parseInt(incomeInputFieldString);
-    console.log(incomeInputFieldValue);
+    const incomeInputFieldValue = getInputValue("incomeInput", "Income");
 
     /* Total Expenses*/
     // get food input value
-    const foodInputField = getExpensesInputValue("foodInput");
+    const foodInputField = getInputValue("foodInput", "Food");
     // get rent input value
-    const rentInputField = getExpensesInputValue("rentInput");
+    const rentInputField = getInputValue("rentInput", "Rent");
     // get clothes input value
-    const clothesInputField = getExpensesInputValue("clothesInput");
+    const clothesInputField = getInputValue("clothesInput", "Clothes");
 
-    // get total expenses
-    const totalExpenses = getTotal("total-expenses");
-    // get total balance
-    const totalBalance = getTotal("total-balance");
+    // calculate total expenses
+    const additionTotalExpenses =
+        foodInputField + rentInputField + clothesInputField;
+
+    // calculate total balance
+    const totalBalance = incomeInputFieldValue - additionTotalExpenses;
+
+    // checking income is less than total expenses or not
+    if (incomeInputFieldValue < additionTotalExpenses) {
+        alert("Total Expenses greater than Income!");
+    } else {
+        // get total expenses
+        getTotal("total-expenses", additionTotalExpenses);
+        // get total balance
+        getTotal("total-balance", totalBalance);
+    }
 });
+
+// saving calculating
+document
+    .getElementById("savingCalculate")
+    .addEventListener("click", function () {
+        // get income value
+        const incomeValue = getInputValue("incomeInput", "Income");
+        // get percentage input value
+        const percentageValue = getInputValue("percentageBtn", "Percentage");
+
+        // checking percentage input value greater than 100 or not
+        if (percentageValue > 100) {
+            alert("Please Provide Percentage Value Greater Than 100");
+        } else {
+            // get saving amount
+            const savingAmount = (incomeValue * percentageValue) / 100;
+            getTotal("savingAmount", savingAmount);
+
+            /* get remaining balance */
+            // get total balance
+            const totalBalance = getTotal("total-balance");
+            const remainingBalance = totalBalance - savingAmount;
+            getTotal("remainingBalance", remainingBalance);
+        }
+    });
--- js/utilites.js
+++ js/utilites.js
@@ -1,19 +1,31 @@
-function getExpensesInputValue(input) {
+function getInputValue(input, errorInputField) {
     // get input field
     const inputField = document.getElementById(input);
     // get input value as string
     const inputString = inputField.value;
-    // get input value as a number
-    const inputFieldValue = parseFloat(inputString);
+    let inputFieldValue;
+    // checking input value number or string or empty
+    if (!isNaN(inputString) && inputString !== "") {
+        // get input value as a number
+        inputFieldValue = parseFloat(inputString);
+    } else {
+        alert(`Please Provide Valid ${errorInputField} Number!`);
+        return 0;
+    }
     return inputFieldValue;
 }
 
-function getTotal(txtField){
+function getTotal(txtField, txtValue) {
     // get text field
     const textField = document.getElementById(txtField);
     // get input value as string
     const textFieldString = textField.innerText;
     // get input value as a number
     const textFieldValue = parseFloat(textFieldString);
+    // checking txtValue has or not
+    if (txtValue) {
+        // added txtValue to the textField
+        textField.innerText = txtValue;
+    }
     return textFieldValue;
 }
